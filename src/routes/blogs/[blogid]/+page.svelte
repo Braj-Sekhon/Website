b01d5a7b25406a257ec7ef596e7509dba7250ed7
Ybg <pdool177@gmail.com>
2025-04-02 10:29:52 -0700

    Ikd mane

--- src/routes/blogs/[blogid]/+page.svelte
+++ src/routes/blogs/[blogid]/+page.svelte
@@ -3,9 +3,20 @@
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
+  // data.text = data.text.replaceAll("\\n", "<br />");
 </script>
 
 <PageWrapper title_text={data.title}>
   <p>Written by {data.author}</p>
   <p>{data.text}</p>
 </PageWrapper>
+
+<style>
+  line {
+    width: 100%;
+    height: 10%;
+    visibility: visible;
+    color: aliceblue;
+    display: block;
+  }
+</style>
